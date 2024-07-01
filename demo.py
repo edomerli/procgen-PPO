Inserted default params, switched to approx KL and value normalization bug fix

demo.py
@@ -21,8 +21,9 @@ supported_difficulties = ["easy", "hard"]
 parser = argparse.ArgumentParser("Script to launch the demo of a trained agent on a selected game and difficulty level")
 game_arg = parser.add_argument("--game", type=str, help=f"Game to play. Supported games are: {supported_games}")
 diff_arg = parser.add_argument("--difficulty", type=str, help=f"Difficulty level of the game. Possible choices are: {supported_difficulties}")
+parser.add_argument('--stack_size', default=4, type=int, help=f"Number of frames to stack together. Default is 4.")
 parser.add_argument('--batch_norm', default=False, type=lambda x: (str(x).lower() == 'true'), help=f"Whether to use batch normalization in the network or not. Default is False.")
-parser.add_argument('--norm_v_targets', default=False, type=lambda x: (str(x).lower() == 'true'), help=f"Whether to normalize the value targets. Default is False.")
+parser.add_argument('--normalize_v_targets', default=True, type=lambda x: (str(x).lower() == 'true'), help=f"Whether to normalize the value targets. Default is True.")
 
 
 args = parser.parse_args()
@@ -69,12 +70,12 @@ env = gym.make(
 
 # create the agent and load the model
 class Config:
-    def __init__(self, batch_norm, normalize_v_targets):
-        self.stack_size = 2
+    def __init__(self, stack_size, batch_norm, normalize_v_targets):
+        self.stack_size = stack_size
         self.batch_norm = batch_norm
         self.normalize_v_targets = normalize_v_targets
 
-config = Config(args.batch_norm, args.normalize_v_targets)
+config = Config(args.stack_size, args.batch_norm, args.normalize_v_targets)
 policy = PPO(env, config)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")

ppo.py
@@ -35,7 +35,7 @@ class ImpalaNetwork(torch.nn.Module):
         self.res_blocks1 = nn.ModuleList()
         self.res_blocks2 = nn.ModuleList()
 
-        hidden_channels = [32, 64, 64]
+        hidden_channels = [16, 32, 32]
 
         for out_channels in hidden_channels:
 
@@ -138,10 +138,6 @@ class PPO:
     def actions_dist_and_v(self, state):
         dist = self.policy_net(state)
         value = self.value_net(state)
-
-        if self.normalize_v_targets:
-            # denormalize value
-            value = value * max(self.value_std, 1e-6) + self.value_mean
 
         return dist, value
       

train.py
@@ -43,6 +43,10 @@ def train(policy, policy_old, train_dataloader, optimizer_policy, optimizer_valu
             # but avoids overflows and division by (potentially if underflown) zero, breaking loss function
             ratios = torch.exp(log_probs - old_log_probs)
 
+            with torch.no_grad():
+                # KL divergence between old and new policy for early stopping
+                kl_div = ((ratios - 1) - (log_probs - old_log_probs)).mean().item()
+
             # clipped surrogate loss
             l_clips = -torch.min(ratios * advantages, torch.clip(ratios, 1-config.eps_clip, 1+config.eps_clip) * advantages)
             loss_pi = torch.mean(l_clips)
@@ -76,11 +80,7 @@ def train(policy, policy_old, train_dataloader, optimizer_policy, optimizer_valu
         if scheduler_value is not None:
             scheduler_value.step()
 
-        with torch.no_grad():
-            # Early stopping: KL divergence between old and new policy, stop training if too high
-            #                 since it means the new policy is too different from the old one
-            kl_div = torch.distributions.kl.kl_divergence(old_dists, dists).mean().item()
-            wandb.log({"train/kl_div": kl_div, "train/batch": utils.global_batch})
-            if kl_div > config.kl_limit:
-                print(f"Early stopping at epoch {epoch} due to KL divergence {round(kl_div, 4)} > {config.kl_limit}")
-                break
+        wandb.log({"train/kl_div": kl_div, "train/batch": utils.global_batch})
+        if kl_div > config.kl_limit:
+            print(f"Early stopping at epoch {epoch} due to KL divergence {round(kl_div, 4)} > {config.kl_limit}")
+            break